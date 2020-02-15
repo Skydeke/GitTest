47a88a09ecac9bfa9f4f77edc69fc007436a0030
David <david-metzler@web.de>
2020-02-15 12:52:52 +0100

    Boom.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Test.");
+        System.out.println("Git-Webhook.");
         System.exit(0);
     }
 }
